eb0656ff360959990306aa264bab982fb2b6f0aa
Genevieve <genevieveappia2@gmail.com>
2025-05-20 09:35:30 +0000

    addition

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,13 @@
+// Declare variable to store navbar element
+const navElement = document.getElementById('navbar');
 // detect scroll event on document
 document.addEventListener('scroll', function() {
 // console.log(window.scrollY);
 if (window.scrollY > 70) {
     // change navbar bg to blue
-    document.getElementById('navbar').style.backgroundColor = '#22629e';
+    navElement.style.backgroundColor = '#22629e';
 }
 else{
     // change navbar bg to transparent
-    document.getElementById('navbar').style.backgroundColor = 'transparent'}
+    navElement.style.backgroundColor = 'transparent'}
 });
